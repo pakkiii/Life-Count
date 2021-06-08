4763ec40ba1148078da05fca2ea3a7907ea74814
pakkiii <80090340+pakkiii@users.noreply.github.com>
2021-06-08 12:52:55 +0900

    first commit

--- app/javascript/yell.js
+++ app/javascript/yell.js
@@ -1,47 +1,15 @@
-const buildHTML = (XHR) => {
-  const item = XHR.response.post;
-  //
-  const html = `
-  <div class="yell-content">
-    ${item.content}
-    ${item.created_at.strftime("%m")}
-  </div>`;
-  return html
+function home() {
+  const items = document.getElementsByClassName("item-img-content");
+  for (let i = 0; i < items.length; i++) {
+    let item = items.item(i);
+    let itemInner = item.getElementsByClassName("item-img-content-z hidden").item(0);
+    item.addEventListener("mouseover", function(){
+      itemInner.setAttribute("style", "display:block;");
+    });
+    item.addEventListener("mouseout", function(){
+      itemInner.removeAttribute("style", "display:block;");
+    });
+  }
+
 }
-
-
-
-
-function post (){
-  const submit = document.getElementById("submit");
-  //submitを定義してこの後に使えるようにしている
-  submit.addEventListener("click", (e) => {
-    //クリックした際のアクションを定義している
-    e.preventDefault();
-    //二重送信を防ぐための記述
-    const form = document.getElementById("form");
-    //index.htmlにて送信フォームで受け取った値を下の行の引数として使うため定義している。
-    const formData = new FormData(form);
-
-    const XHR = new XMLHttpRequest();
-
-    XHR.open("POST", url, true);
-
-    XHR.responseType = "json";
-    //JavaScriptの値としてレスポンスを受け取るため事前に定義している
-    XHR.send(formData);
-    //
-    XHR.onload = () => {
-      if (XHR.status != 200) {
-        alert(`Error ${XHR.status}: ${XHR.statusText}`);
-        return null;
-      }
-      const list = document.getElementById("list");
-      const formText = document.getElementById("content")
-      list.insertAdjacentHTML("afterend", buildHTML(XHR));
-      formText.value = "";
-    };
-  });
-}
-
-window.addEventListener('load', post);
+window.addEventListener('load', home);
